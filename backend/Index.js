6060ab9bf07a880430faa40202df061d407134a6
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-02-28 22:30:22 +0200

    Added Mongodb connection

--- backend/Index.js
+++ backend/Index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.Mongo).then( () => {
+    console.log("connected to Mongodb");
+})
+.catch( (err) => {
+    console.log(err);
+});
 
 const app = express();
 
