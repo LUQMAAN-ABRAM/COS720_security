acfa08ec326970146ea33205c1fb8b5d6022cd57
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-02-28 16:16:45 +0200

    Added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/SignUp" element={<SignUp />}/>
+      <Route path="/Login" element={<Login />}/>
+      <Route path="/About" element={<About />}/>
+      <Route path="/Profile" element={<Profile />}/>
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
