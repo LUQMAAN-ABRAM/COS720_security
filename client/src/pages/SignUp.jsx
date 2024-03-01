e5d76bacd7eea7826fc521436b6a3525a92f688a
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-03-01 16:38:47 +0200

    added signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+  const [formdata, setformdata] = useState({});
+  const handlechange = (e) => {
+    setformdata({...formdata, [e.target.id]: e.target.value
+    });
+  };
+
+  const handlesubmit = async(e) =>{
+    e.preventDefault();
+    const res = await fetch('http://localhost:3000/backend/auth/signup', formdata);
+    const data = await res.json();
+    console.log(data);
+    
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-      <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg'/>
-        <input type='email' placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg'/>
-        <input type='password' placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg'/>
+
+      <form className='flex flex-col gap-4' onSubmit={handlesubmit}>
+        <input
+         type='text'
+         placeholder='Username'
+         id='username'
+         className='bg-slate-100 p-3 rounded-lg' 
+         onChange={handlechange}/>
+
+        <input
+         type='email'
+         placeholder='Email'
+         id='email'
+         className='bg-slate-100 p-3 rounded-lg'
+         onChange={handlechange}/>
+
+        <input 
+        type='password' 
+        placeholder='Password' 
+        id='password' 
+        className='bg-slate-100 p-3 rounded-lg'
+        onChange={handlechange}/>
 
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Submit</button>
       </form>
