f0b05d23274853a510f75af10695e4049e3743d3
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-03-01 23:21:19 +0200

    added token

--- backend/controllers/Auth.controller.js
+++ backend/controllers/Auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const {username, email, password} = req.body;
@@ -17,3 +18,18 @@ export const signup = async (req, res, next) => {
   }
   
  };
+
+ export const login = async (req, res, next) => {
+  const {email, password} = req.body;
+  try {
+    const validuser = await User.findOne({email});
+    if (!validuser) return next(errorhandler(404, 'User not found'));
+    const validpassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validpassword) return next(errorhandler(401, 'Incorrect credentials'));
+    const token = jwt.sign({id: validuser._id}, process.env.JWT_SECRET );
+    const {password: hashedpassword, ...rest} = validuser._doc;
+    res.cookie('access token', token, {httpOnly:true}).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+ }
