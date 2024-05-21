0cc489d4fa82d6a4e3335c569314d5f9ce0b0541
Luqmaan Abram <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-05-21 21:52:06 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import Modules from './pages/Modules';
 
+
 export default function App() {
   return (
     <BrowserRouter>
