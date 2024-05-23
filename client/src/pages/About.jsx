102db68b4673efe5b0023894cf05e7b3cdcec8e2
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-05-23 15:08:20 +0200

    background

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white"
+      style={{
+        backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtPMl2_eUx2WMeEy0FVEXZjWbjNZZrmgdLrA&s')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
       <div className="max-w-lg p-8 rounded-lg bg-gray-800 shadow-lg">
         <h1 className="text-3xl font-bold text-white mb-4">Security Monitoring</h1>
         <p className="text-white mb-6">
