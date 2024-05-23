10b8d418e86536fff842ca15fdaf1762fce1540e
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-05-23 15:24:58 +0200

    Add preproduction

--- backend/Index.js
+++ backend/Index.js
@@ -5,6 +5,7 @@ import UserRoutes from './routes/User.route.js';
 import AuthRoutes from './routes/Auth.route.js';
 import ModuleRoutes from './routes/Module.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.Mongo).then( () => {
@@ -14,7 +15,17 @@ mongoose.connect(process.env.Mongo).then( () => {
     console.log(err);
 });
 
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+
 app.use(express.json());
 app.use(cookieParser());
 
