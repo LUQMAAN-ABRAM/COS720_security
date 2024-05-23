2430eeca986d3f524041be1e8e498f5ddfe3e9bc
LUQMAAN-ABRAM <104630753+LUQMAAN-ABRAM@users.noreply.github.com>
2024-05-23 12:39:31 +0200

    change ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div >Home </div>
-  )
-}
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white"
+      style={{
+        backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSCWv9VdpGtT1Ah241Sqv8TnDB3qf9Q5WFjQ&s')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="text-4xl font-bold mb-6">Welcome to the COS720 Cloud Security Project</div>
+      <p className="text-lg mb-8">Try our cutting-edge modules.</p>
+      <Link
+        to="/Modules"
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        Choose Your Modules
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
